43c4740bd1c9b3adb7db182b7eeb1b9af83c54a3
Siguretuki <101462925+Siguretuki@users.noreply.github.com>
2023-01-31 02:31:12 +0000

    a

--- components/appfooter.vue
+++ components/appfooter.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="text-center bg-light" id="footer">
-        <footer class="p-2 pb-2">{{data.footer}}</footer>
+        <footer class="p-4 pb-2">{{data.footer}}</footer>
         <div class="row justify-content-center">
-            <div id="link" class="col-1" v-for="item in data.sns_link">
-                <a class="btn btn-outline-primary btn-lg" :href="item.text" type="button">
+            <div id="link" class="col-1 p-4" v-for="item in data.sns_link">
+                <div class="btn btn-outline-primary btn-lg" type="button" >
+                    <a :href="item.text"  style="text-decoration:none;">
                     {{ item.name }}
-                </a>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
