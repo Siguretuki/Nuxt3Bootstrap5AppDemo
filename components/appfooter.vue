132176f74ec8be050656d7adc6ebeb2301378fda
Siguretuki <101462925+Siguretuki@users.noreply.github.com>
2023-01-31 02:17:44 +0000

    a

--- components/appfooter.vue
+++ components/appfooter.vue
@@ -3,7 +3,7 @@
         <footer class="p-2 pb-2">{{data.footer}}</footer>
         <div class="row justify-content-center">
             <div id="link" class="col-1" v-for="item in data.sns_link">
-                <a class="btn btn-primary btn-lg" :href="item.text" role="button">
+                <a class="btn btn-outline-primary btn-lg" :href="item.text" type="button">
                     {{ item.name }}
                 </a>
             </div>
